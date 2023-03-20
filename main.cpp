1eea916df03caa81b4e3bdb4da24db0806e6815e
Jakub Majewski <jakub.majewski.programista@gmail.com>
2023-03-20 10:30:15 +0100

    Add registerd user to vector

--- main.cpp
+++ main.cpp
@@ -6,5 +6,11 @@ using namespace std;
 int main()
 {
     KsiazkaAdresowa ksiazkaAdresowa;
+    for (int i = 0; i < 2; i++)
+    {
+      ksiazkaAdresowa.rejestracjaUzytkownika();
+    }
+    //ksiazkaAdresowa.rejestracjaUzytkownika();
+    ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
     return 0;
 }
